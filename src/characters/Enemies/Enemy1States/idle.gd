8d608b31f3ea85a8cc9ebf11eaced741534b375c
Filip Jastrzebski <1p22jastrzebski@gmail.com>
2026-01-20 17:23:16 +0100

    wstepna scena i stany przeciwnika

---
+++ src/characters/Enemies/Enemy1States/idle.gd
@@ -0,0 +1,11 @@
+extends State
+
+@onready var walk_component: Node3D = $"../../WalkComponent"
+
+var direction := Vector3.ZERO
+
+func Update(_delta: float):
+	
+
+func Physics_Update(_delta: float):
+	walk_component.
