afacd3c58620236cfc9c890036bdcc3ee6109374
KubaK1P <1p22kus@gmail.com>
2026-03-21 19:36:15 +0100

    feat(main menu): audio test

--- scenes/Boot/Boot.gd
+++ scenes/Boot/Boot.gd
@@ -6,18 +6,10 @@ const MAIN_MENU_SCENE = "res://scenes/UI/MainMenu/MainMenu.tscn"
 
 
 func _ready() -> void:
-	# Access the AudioController singleton and interact with it
-	print("--- Boot.gd interacting with AudioController ---")
-	print("Initial Master Volume from Boot.gd: ", AudioController.master_volume)
-	print("Initial SFX Volume from Boot.gd: ", AudioController.sfx_volume)
-
-	AudioController.set_master_volume(0.5)
+	AudioController.set_master_volume(0.8)
 	AudioController.set_sfx_volume(0.8)
-	AudioController.play_sfx("res://assets/AUDIO/sfx/lick_tenor_sax.mp3")
-
-	print("New Master Volume from Boot.gd: ", AudioController.master_volume)
-	print("New SFX Volume from Boot.gd: ", AudioController.sfx_volume)
-	print("--- End Boot.gd interaction ---")
+	AudioController.set_environment_volume(0.67)
+	
 
 	if skip_boot_scene and OS.is_debug_build():
 		get_tree().change_scene_to_file.call_deferred(MAIN_MENU_SCENE)
