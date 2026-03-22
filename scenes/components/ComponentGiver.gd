2fc504f036a90b3ff1c167535ff16323c7987e90
KubaK1P <1p22kus@gmail.com>
2026-03-22 19:33:41 +0100

    fix: ComponentGiver now calls acquire_component on ComponentManager

--- scenes/components/ComponentGiver.gd
+++ scenes/components/ComponentGiver.gd
@@ -3,15 +3,18 @@ extends Area3D
 @export var component_resource: Resource
 
 func _on_body_entered(body):
-    print("Body entered ComponentGiver: ", body.name, " group: ", body.get_groups())
+	print("Body entered ComponentGiver: ", body.name, " group: ", body.get_groups())
 	if body.is_in_group("player"):
 		print("Player entered ComponentGiver!")
-		var player = body
-		if player.has_method("acquire_component"):
-			print("Player has acquire_component method. Acquiring component...")
-			player.acquire_component(component_resource)
+		var player_node = body # The CharacterBody3D node
+		# Find the ComponentManager child node
+		var component_manager = player_node.find_child("ComponentManager")
+		
+		if component_manager and component_manager.has_method("acquire_component"):
+			print("ComponentManager found. Acquiring component...")
+			component_manager.acquire_component(component_resource)
 			queue_free()
 		else:
-			print("Player DOES NOT have acquire_component method.")
+			print("ComponentManager or acquire_component method not found on player's child.")
 	else:
 		print("Body is not player.")
